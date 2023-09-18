8cd4e26d2d19ebefa63fead02dbc8417502462e1
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-09-18 00:45:15 -0400

    Time: 5 ms (50.00%), Space: 7.1 MB (25.00%) - LeetHub

---
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for ImmutableListNode.
+ * struct ImmutableListNode {
+ *     struct ImmutableListNode* (*getNext)(struct ImmutableListNode*); // return the next node.
+ *     void (*printValue)(struct ImmutableListNode*); // print the value of the node.
+ * };
+ */
+
+void printLinkedListInReverse(struct ImmutableListNode* head) {
+        /* reached end of list */
+        if(head == NULL) {
+            return;
+        }
+        printLinkedListInReverse(head->getNext(head));
+        head->printValue(head);
+}
