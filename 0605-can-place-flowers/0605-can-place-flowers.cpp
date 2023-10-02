a5e81cc9684de3da0a3706bc09bc49a7fcb437e8
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-10-01 18:57:53 -0700

    Time: 3 ms (99.77%), Space: 20.5 MB (32.03%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (flowerbed.size() == 0u) {
+            return false;
+        }
+
+        int count = 0u;
+        for (int i = 0u; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 0u) {
+                /* check if can be inserted */
+                bool left_empty = (i == 0u) ? true : (flowerbed[i - 1u] == 0u);
+                bool right_empty = (i == flowerbed.size() - 1u) ? true : (flowerbed[i + 1u] == 0u);
+                if (left_empty && right_empty) {
+                    flowerbed[i] = 1u;
+                    count++;
+                }
+            }
+        }
+        return (n <= count);
+    }
+};
