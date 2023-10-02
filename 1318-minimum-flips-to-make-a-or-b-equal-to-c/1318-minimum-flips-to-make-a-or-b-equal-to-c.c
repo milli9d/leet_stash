d41d320215fa6a1bdaa44a49d508438a828bdb3c
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-10-01 19:36:12 -0700

    Time: 2 ms (61.33%), Space: 6.4 MB (9.33%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.c
@@ -0,0 +1,12 @@
+uint32_t count_bits(uint32_t n) { 
+    int out = 0u;
+    while(n) {
+        n = n & (n - 1);
+        out++;
+    }
+    return out;
+}
+
+int minFlips(int a, int b, int c) {
+    return count_bits((a | b) ^ c) + count_bits(a & b & ((a | b) ^ c));    
+}
