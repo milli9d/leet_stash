f9c2247508e37eb3b50199fb9b3fc59e45d82716
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-10-01 18:59:05 -0700

    Time: 20 ms (8.82%), Space: 20.6 MB (22.13%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -14,6 +14,7 @@ class Solution {
                 if (left_empty && right_empty) {
                     flowerbed[i] = 1u;
                     count++;
+                    if(count >= n) { return true; }
                 }
             }
         }
