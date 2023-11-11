a0c80a8d6bd3acb7a74f6fd7d595b6314270493a
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-11-11 16:11:06 -0500

    Time: 75 ms (98.95%), Space: 70 MB (47.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,28 @@
+// Boost i/o.
+auto init = [](){ 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
+class Solution
+{
+    public:
+
+        /**
+         *@brief 
+         */
+        bool containsDuplicate(vector<int> &nums)
+        {            
+            std::unordered_set<int> _memo{};
+            for(int i : nums) {
+                if(_memo.find(i) != _memo.end()) {
+                    return true;
+                } else {
+                    _memo.insert(i);
+                }
+            }
+            return false;
+        }
+};
