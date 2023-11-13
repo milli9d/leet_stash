99ddf61e4d478a4940cd547b8bfb9bb621dff467
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-11-13 00:09:20 -0500

    Time: 12 ms (63.01%), Space: 14.1 MB (43.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -11,16 +11,11 @@ public:
         
         priority_queue<std::pair<int,int>> _pq{};
         
-        for(auto i : _map) {
-            
+        for(auto i : _map) {    
             _pq.push({i.second, i.first});
-        
-            cout << i.first << " : " << i.second << endl;
-        
         }
 
         std::vector<int> out{};
-        
         for(int i = 0 ; i < k ; i++) {
             out.push_back(_pq.top().second);
             _pq.pop();
