a7b2226fc9a0641cccbf41c9a28bcf17d6a7cecc
Milind Singh <48103844+milli9d@users.noreply.github.com>
2023-12-19 23:14:50 -0800

    Time: 98 ms, Memory: 110.1 MB - LeetHub

---
+++ maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        ios::sync_with_stdio(false);
+
+        double sum = 0.0;
+        for(int i = 0 ; i < k ; i++) {
+            sum += nums[i];
+        }
+
+        double max_sum{sum};
+        for(int i = k ; i < nums.size() ; i++) {
+            sum += (nums[i] - nums[i - k]);
+            max_sum = max(sum, max_sum);
+        }
+
+        return static_cast<double>(max_sum / k);
+
+    }
+};
