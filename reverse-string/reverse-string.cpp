945921740ec7a2a21023d742b6d9d7303f0fa589
Milind Singh <48103844+milli9d@users.noreply.github.com>
2025-02-09 11:04:13 -0800

    Time: 0 ms, Memory: 27 MB - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        /* reverse by swapping elements */
-        size_t _sz = s.size();
-        for(size_t i = 0u ; i < _sz/2u ; i++) {
-            std::swap(s[i], s[_sz - i - 1]);
-        } 
+        for(auto i = 0 ; i < s.size() / 2 ; i++) {
+            std::swap(s[i], s[s.size() - i - 1u]);
+        }  
+        
     }
 };
